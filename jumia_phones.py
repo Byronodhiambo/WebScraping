5119cd1e1cdac64896702c86575d0aaf3d276e7c
Byronodhiambo <byronjonnes25@gmail.com>
2022-03-23 23:54:08 +0300

    jumiphones

--- jumia_phones.py
+++ jumia_phones.py
@@ -3,30 +3,45 @@ from turtle import delay
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 
-myurl = 'https://www.jumia.co.ke/mobile-phones/'
+myurl = ['https://www.jumia.co.ke/mobile-phones/''https://www.jumia.co.ke/mobile-phones/?page=2#catalog-listing', 'https://www.jumia.co.ke/mobile-phones/?page=3#catalog-listing', 'https://www.jumia.co.ke/mobile-phones/?page=4#catalog-listing'] 
 
-# check if reachable
-session = requests.Session()
-response = session.get(myurl, headers={'User-Agent': 'Mozilla/5.0'})
-print(response.status_code)
+for url in myurl:
+    # # reachable
+    # session = requests.Session()
+    # response = session.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+    # print(response.status_code)
 
-# connect & grab page
-req = Request(myurl, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'})
-client = urlopen(req, timeout=10)
-web_page = client.read()
-client.close()
+    # connect & grab page
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'})
+    client = urlopen(req, timeout=10)
+    web_page = client.read()
+    client.close()
 
-soup = BeautifulSoup(web_page, 'html.parser')
-phones_cont = soup.find_all("a",{"class":"core"})
+    soup = BeautifulSoup(web_page, 'html.parser')
+    phones_cont = soup.find_all("a",{"class":"core"})
 
-# print(len(phones))
+    print(len(phones_cont))
+    count = 0
 
-for cont in phones_cont:
-    brand = cont['data-brand']
-    desc = cont['data-name']
-    price = cont.find("div", {"class":"prc"}).get_text()
-    disc = cont.find("div", {"class":"tag _dsct"}).get_text()    
-    print(brand + " " + desc + " " + price + " " + disc )
+    for cont in phones_cont:
+        count = count+1        
+        try:
+            brand = cont['data-brand']
+            desc = cont['data-name']
+            price = cont.find("div", {"class":"prc"}).get_text()
+            if (cont.find("div", {"class":"tag _dsct"}) == None):
+                disc =cont.find("div", {"class":"tag _dsct _sm"}).get_text()
+            else:
+                disc = cont.find("div", {"class":"tag _dsct"}).get_text() 
+            rating = cont.find("div", {"class":"stars _s"}).get_text()
 
+        except:
+            brand = 'None'
+            desc = 'None'
+            disc = "Not available"
+            price = 'None'
+            rating = 'None'
 
-# print(page_soup.header.prettify())
+        print(count , " " + brand + " " + desc + " " + price + " " + disc + " " + rating)
+
+    # print(page_soup.header.prettify())
