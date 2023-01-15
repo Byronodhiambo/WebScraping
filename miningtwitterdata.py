0eb130dd63a08a94dfcdcb2a0492b2b03cc1fbb2
Byronodhiambo <byronjonnes25@gmail.com>
2023-01-15 21:15:29 +0300

    mining twitter data

--- miningtwitterdata.py
+++ miningtwitterdata.py
@@ -1,5 +1,6 @@
-import tweepy
+import tweepy, Stream
 from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
  
 consumer_key = 'CONSUMER-KEY'
 consumer_secret = 'CONSUMER-SECRET'
@@ -12,7 +13,35 @@ auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
 
 
-
+# homepage tweets
 for status in tweepy.Cursor(api.home_timeline).items(10):
-    # Process a single status
     print(status.text)
+
+# all followers
+for friend in tweepy.Cursor(api.friends).items():
+    store(friend._json)
+
+def store(tweet):
+    print(json.dumps(tweet))
+
+
+
+
+# streaming all updated tweets
+class MyListener(StreamListener):
+ 
+    def on_data(self, data):
+        try:
+            with open('python.json', 'a') as f:
+                f.write(data)
+                return True
+        except BaseException as e:
+            print("Error on_data: %s" % str(e))
+        return True
+ 
+    def on_error(self, status):
+        print(status)
+        return True
+ 
+twitter_stream = Stream(auth, MyListener())
+twitter_stream.filter(track=['#python'])
